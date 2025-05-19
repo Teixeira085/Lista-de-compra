bee7d222fc12187bc6123aa889628b7d1e8fd745
Rafael teixeira <Teixeiraflavia842@gmail.com>
2025-05-19 14:15:16 -0300

    lista-de-compras

---
+++ Lista de compra/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Lista_de_compra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblItem = new System.Windows.Forms.Label();
+            this.txtItem = new System.Windows.Forms.TextBox();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.lstCompras = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblItem
+            // 
+            this.lblItem.AutoSize = true;
+            this.lblItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblItem.Location = new System.Drawing.Point(165, 46);
+            this.lblItem.Name = "lblItem";
+            this.lblItem.Size = new System.Drawing.Size(79, 31);
+            this.lblItem.TabIndex = 0;
+            this.lblItem.Text = "ITEM";
+            // 
+            // txtItem
+            // 
+            this.txtItem.Location = new System.Drawing.Point(151, 106);
+            this.txtItem.Name = "txtItem";
+            this.txtItem.Size = new System.Drawing.Size(110, 20);
+            this.txtItem.TabIndex = 1;
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnAdicionar.ForeColor = System.Drawing.Color.Lime;
+            this.btnAdicionar.Location = new System.Drawing.Point(38, 162);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(125, 50);
+            this.btnAdicionar.TabIndex = 2;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = false;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnRemover.ForeColor = System.Drawing.Color.Blue;
+            this.btnRemover.Location = new System.Drawing.Point(309, 160);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(119, 57);
+            this.btnRemover.TabIndex = 3;
+            this.btnRemover.Text = "Remover Selecionado ";
+            this.btnRemover.UseVisualStyleBackColor = false;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click_1);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnLimpar.ForeColor = System.Drawing.Color.Red;
+            this.btnLimpar.Location = new System.Drawing.Point(179, 164);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(104, 48);
+            this.btnLimpar.TabIndex = 4;
+            this.btnLimpar.Text = "Limpar ";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click_1);
+            // 
+            // lstCompras
+            // 
+            this.lstCompras.FormattingEnabled = true;
+            this.lstCompras.Location = new System.Drawing.Point(135, 247);
+            this.lstCompras.Name = "lstCompras";
+            this.lstCompras.Size = new System.Drawing.Size(109, 82);
+            this.lstCompras.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Lista_de_compra.Properties.Resources.istockphoto_1412353022_612x612;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(554, 377);
+            this.Controls.Add(this.lstCompras);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.txtItem);
+            this.Controls.Add(this.lblItem);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "Lista de Compras ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblItem;
+        private System.Windows.Forms.TextBox txtItem;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.ListBox lstCompras;
+    }
+}
+
---
+++ Lista de compra/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lista_de_compra
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            string item = txtItem.Text.Trim();
+
+            if (string.IsNullOrEmpty(item))
+            {
+                MessageBox.Show("Digite um item para adicionar.", "Erro",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            lstCompras.Items.Add(item);
+            txtItem.Clear();
+            txtItem.Focus();
+        }
+
+
+        private void btnRemover_Click_1(object sender, EventArgs e)
+        {
+            if (lstCompras.SelectedIndex != -1)
+            {
+                lstCompras.Items.RemoveAt(lstCompras.SelectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("Selecione um item para remover.", "Aviso");
+            }
+
+        }
+
+        private void btnLimpar_Click_1(object sender, EventArgs e)
+        {
+            if (lstCompras.Items.Count == 0)
+            {
+                MessageBox.Show("A lista já está vazia.", "Informação",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            DialogResult result = MessageBox.Show("Deseja realmente limpar a lista?",
+                "Confirmação", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                lstCompras.Items.Clear();
+            }
+
+        }
+    }
+}
+
+
+
+
+
